ae3f21700326d433d8c68ae0306b2b6d9c2dd4a6
alekseyleshko <alekseyleshko@gmail.com>
2014-07-15 13:21:43 +0400

    Add slim

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var slim = require("gulp-slim");
+
+gulp.task('slim', ['slim-main', 'slim-partials']);
+
+gulp.task('slim-main', function() {
+  gulp.src("./src/slim/*.slim")
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest("./www/"));
+});
+
+gulp.task('slim-partials', function() {
+  gulp.src("./src/slim/partials/*.slim")
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest("./www/partials/"));
+});
 
 gulp.task('default', function() {
-    // place code for your default task here
+  // place code for your default task here
 });

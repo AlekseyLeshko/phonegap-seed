c8d4a5c8efc2c053c1a4b23dd7f8f60d05816354
alekseyleshko <alekseyleshko@gmail.com>
2014-08-02 15:53:39 +0400

    Add tests: unit and e2e

---
+++ test/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'src/bower_components/angular/angular.js',
+      'src/bower_components/angular-route/angular-route.js',
+      'src/bower_components/angular-mocks/angular-mocks.js',
+      'src/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};

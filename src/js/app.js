f68fd62377abb5baa263c1ff337e61f6b10a4820
alekseyleshko <alekseyleshko@gmail.com>
2014-08-15 10:43:55 +0400

    Configure jshint

--- src/js/app.js
+++ src/js/app.js
@@ -22,28 +22,28 @@ angular.module('myApp', [
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('tabs', {
-      url: "/tab",
+      url: '/tab' ,
       abstract: true,
-      templateUrl: "partials/tabs.html"
+      templateUrl: 'partials/tabs.html'
     })
     .state('tabs.view1', {
-      url: "/view1",
+      url: '/view1',
       views: {
         'view1-tab': {
-          templateUrl: "partials/partial1.html",
+          templateUrl: 'partials/partial1.html',
           controller: 'MyCtrl1'
         }
       }
     })
     .state('tabs.view2', {
-      url: "/view2",
+      url: '/view2',
       views: {
         'view2-tab': {
-          templateUrl: "partials/partial2.html",
+          templateUrl: 'partials/partial2.html',
           controller: 'MyCtrl2'
         }
       }
     });
 
-   $urlRouterProvider.otherwise("/tab/view1");
-})
+   $urlRouterProvider.otherwise('/tab/view1');
+});

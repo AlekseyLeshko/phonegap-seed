ed897d9d48e6d98bd44e01093cbd8c7ea4e7bdb5
alekseyleshko <alekseyleshko@gmail.com>
2014-08-15 15:17:33 +0400

    Fix file names in unit test

--- test/unit/directives.spec.js
+++ test/unit/directives.spec.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* jasmine specs for directives go here */
-
 describe('directives', function() {
   beforeEach(module('myApp.directives'));
 
--- test/unit/services.spec.js
+++ test/unit/services.spec.js
@@ -1,10 +1,7 @@
 'use strict';
 
-/* jasmine specs for services go here */
-
 describe('service', function() {
   beforeEach(module('myApp.services'));
-
 
   describe('version', function() {
     it('should return current version', inject(function(version) {

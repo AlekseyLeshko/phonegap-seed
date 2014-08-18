22e682751dd5924c4ccc42a17c8a5b3fafe577bb
alekseyleshko <alekseyleshko@gmail.com>
2014-08-18 16:26:47 +0400

    Add bower gulp task

---
+++ gulp/tasks/util/bower.js
@@ -0,0 +1,9 @@
+var bower = require('bower');
+var gutil = require('gulp-util');
+
+gulp.task('bower', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});

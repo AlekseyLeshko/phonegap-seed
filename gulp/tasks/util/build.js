6699ef297e335754c61ddbfc25603528c6fc0dbb
alekseyleshko <alekseyleshko@gmail.com>
2014-08-18 16:45:59 +0400

    Fix

--- gulp/tasks/util/build.js
+++ gulp/tasks/util/build.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('build', function(callback) {
-  return runSequence('clean', 'bower', ['test-single-run', 'slim', 'scripts', 'ionic', 'img'], 'scss', callback);
+  return runSequence('clean', 'bower', ['slim', 'scripts', 'ionic', 'img'], 'scss', callback);
 });

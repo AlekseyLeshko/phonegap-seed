3e3a3f4ebe832d4bd39559c0c40abbe21624f36e
alekseyleshko <alekseyleshko@gmail.com>
2014-08-18 16:25:11 +0400

    Add cordova config

--- gulp/tasks/util/install-platforms.js
+++ gulp/tasks/util/install-platforms.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sh = require('shelljs');
+var config = require('../../configs/cordova.json');
 
 gulp.task('install-platforms', function() {
-  var platforms = cordovaConfig.platforms;
+  var platforms = config.platforms;
   for (var i = 0; i < platforms.length; i++) {
     var platform = platforms[i];
     var command = 'cordova platform add ' + platform;
--- gulp/tasks/util/install-plugins.js
+++ gulp/tasks/util/install-plugins.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sh = require('shelljs');
+var config = require('../../configs/cordova.json');
 
 gulp.task('install-plugins', function() {
-  var plugins = cordovaConfig.plugins;
+  var plugins = config.plugins;
   for (var i = 0; i < plugins.length; i++) {
     var plugin = plugins[i];
     var command = 'cordova plugin add ' + plugin;

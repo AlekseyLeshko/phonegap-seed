b007f804146709d5dc7a15b61127b2958de59409
alekseyleshko <alekseyleshko@gmail.com>
2014-08-18 15:55:15 +0400

    Add scss gulp task

---
+++ gulp/tasks/css/scss.js
@@ -0,0 +1,12 @@
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+
+gulp.task('scss', function() {
+  return gulp.src('app/assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(minifyCSS({
+      keepSpecialComments: 0
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('www/css/'));
+});

57d8487e49c1f8e1fc29d8d2ec7251572c90daae
alekseyleshko <alekseyleshko@gmail.com>
2014-08-19 17:10:57 +0400

    Update slim-main gulp task, task add script for debug

--- gulp/tasks/html/slim-main.js
+++ gulp/tasks/html/slim-main.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var slim = require('gulp-slim');
 var minifyHTML = require('gulp-minify-html');
+var htmlreplace = require('gulp-html-replace');
+var config = require('../../configs/gulp.json');
 
 gulp.task('slim-main', function() {
   var slimConfig = {
     pretty: true
+  };
+
+  var url = '"http://';
+  url += config.debug.ip + ':' + config.debug.port;
+  url += '/target/target-script-min.js#anonymous"';
+  var tpl = '<script src=%s></script>';
+
+  var htmlreplaceConfig = {
+    jsForDebug: {
+      src: url,
+      tpl: tpl
+    }
   };
 
   var minifyConfig = {
@@ -13,6 +27,7 @@ gulp.task('slim-main', function() {
 
   return gulp.src('app/*.slim')
     .pipe(slim(slimConfig))
+    .pipe(htmlreplace(htmlreplaceConfig))
     .pipe(minifyHTML(minifyConfig))
     .pipe(gulp.dest('www/'));
 });

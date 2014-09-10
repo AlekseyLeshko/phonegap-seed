5ebf9e62f5dd9814ae836c35e7c389180fb5bab0
alekseyleshko <alekseyleshko@gmail.com>
2014-09-10 13:05:56 +0400

    Add jasmine-jquery

--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -19,4 +19,15 @@ describe('controllers', function(){
       expect(url).toBeDefined();
     })
   });
+
+  describe('fixtures', function() {
+    beforeEach(function() {
+      jasmine.getJSONFixtures().fixturesPath = 'base/test/fixtures';
+    });
+
+    it('should return fixture', function() {
+      var obj = getJSONFixture('test_fixture.json');
+      expect(obj.fixtures).toEqual('load');
+    })
+  });
 });

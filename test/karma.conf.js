3e84c645082f06f90cfb0e2b0e590e379fc5a9d8
alekseyleshko <alekseyleshko@gmail.com>
2014-09-10 13:22:55 +0400

    Update karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,61 +1,7 @@
-module.exports = function(config){
-  config.set({
+var karmaConfig = require('../gulp/configs/karma.json');
 
-    basePath : '../',
+module.exports = function(config) {
+  karmaConfig.basePath = '../';
 
-    files : [
-      'bower_components/ionic/js/ionic.bundle.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'app/**/*.js',
-      'test/unit/**/*.js'
-    ],
-
-    autoWatch : true,
-    usePolling: true,
-
-    frameworks: ['jasmine'],
-
-    browsers : [
-      'Chrome',
-      'PhantomJS',
-      'Firefox',
-      'FirefoxAurora',
-      'FirefoxNightly'
-    ],
-
-    plugins : [
-      'karma-phantomjs-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine',
-      'karma-coverage'
-    ],
-
-    reporters: ['progress', 'coverage'],
-
-    preprocessors: {
-      'src/js/**/*.js': ['coverage']
-    },
-
-    coverageReporter: {
-      reporters:[
-        {
-          type: 'html',
-          dir:'coverage/'
-        }
-      ]
-    },
-
-    customLaunchers: {
-      'PhantomJS_custom': {
-        base: 'PhantomJS',
-        options: {
-          windowName: 'my-window',
-          settings: {
-            webSecurityEnabled: false
-          }
-        },
-        flags: ['--remote-debugger-port=9000']
-      }
-    }
-  });
+  config.set(karmaConfig);
 };

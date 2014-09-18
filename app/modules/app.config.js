54751ec83f69a9bb5c067dfb5a0c6da58958bd51
alekseyleshko <alekseyleshko@gmail.com>
2014-09-18 19:38:20 +0400

    Trash

--- app/modules/app.config.js
+++ app/modules/app.config.js
@@ -5,20 +5,37 @@
     .module('app')
     .config(config);
 
-  config.$inject = ['$routeProvider'];
+  config.$inject = ['$routeProvider', '$translateProvider'];
 
-  function config($routeProvider) {
+  function config($routeProvider, $translateProvider) {
     $routeProvider.
-      when('/view1', {
+      when('/:lang/view1', {
         templateUrl: 'views/partial1.html',
         controller: 'MyCtrl1 as myCtrl1'
       }).
-      when('/view2', {
+      when('/:lang/view2', {
         templateUrl: 'views/partial2.html',
         controller: 'MyCtrl2 as myCtrl2'
       }).
       otherwise({
-        redirectTo: '/view1'
+        redirectTo: 'en/view1'
       });
+
+    $translateProvider.translations('en', {
+      'TITLE': 'Hello',
+      'FOO': 'This is a paragraph'
+    });
+
+    $translateProvider.translations('de', {
+      'TITLE': 'Hallo',
+      'FOO': 'Dies ist ein Paragraph'
+    });
+
+    $translateProvider.translations('ru', {
+      'TITLE': 'Привет',
+      'FOO': 'Это параграф'
+    });
+
+    $translateProvider.preferredLanguage('en');
   }
 })();

82104d87bbd49b6e150b62389aa7a6534f184b97
alekseyleshko <alekseyleshko@gmail.com>
2014-09-18 11:44:10 +0400

    Fix route

--- app/modules/app.config.js
+++ app/modules/app.config.js
@@ -5,34 +5,20 @@
     .module('app')
     .config(config);
 
-  config.$inject = ['$stateProvider', '$urlRouterProvider'];
+  config.$inject = ['$routeProvider'];
 
-  function config($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('tabs', {
-        url: '/tab' ,
-        abstract: true,
-        templateUrl: 'views/tabs.html'
-      })
-      .state('tabs.view1', {
-        url: '/view1',
-        views: {
-          'view1-tab': {
-            templateUrl: 'views/partial1.html',
-            controller: 'MyCtrl1 as myCtrl1'
-          }
-        }
-      })
-      .state('tabs.view2', {
-        url: '/view2',
-        views: {
-          'view2-tab': {
-            templateUrl: 'views/partial2.html',
-            controller: 'MyCtrl2 as myCtrl2'
-          }
-        }
+  function config($routeProvider) {
+    $routeProvider.
+      when('/view1', {
+        templateUrl: 'views/partial1.html',
+        controller: 'MyCtrl1 as myCtrl1'
+      }).
+      when('/view2', {
+        templateUrl: 'views/partial2.html',
+        controller: 'MyCtrl2 as myCtrl2'
+      }).
+      otherwise({
+        redirectTo: '/view1'
       });
-
-    $urlRouterProvider.otherwise('/tab/view1');
   }
 })();

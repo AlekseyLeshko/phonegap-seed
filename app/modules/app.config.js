aad29384dd1c13518aa607b00cb98cd789fc77a7
alekseyleshko <alekseyleshko@gmail.com>
2014-09-19 19:14:05 +0400

    Update app config

--- app/modules/app.config.js
+++ app/modules/app.config.js
@@ -5,50 +5,67 @@
     .module('app')
     .config(config);
 
-  config.$inject = ['$stateProvider', '$urlRouterProvider', '$translateProvider'];
+  config.$inject = [
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$translateProvider'
+  ];
 
   function config($stateProvider, $urlRouterProvider, $translateProvider) {
-    var defaultLang = 'en';
-    $urlRouterProvider.otherwise('/' + defaultLang + '/view/1');
+    createRouting();
+    createI18n();
 
-    $stateProvider
-      .state('lang', {
-        abstract: true,
-        url: '/:lang',
-        controller: 'LangCtrl as langCtrl',
-        template: '<ui-view/>'
-      })
-      .state('lang.view', {
-        abstract: true,
-        url: '/view',
-        template: '<ui-view/>'
-      })
-      .state('lang.view.first', {
-        url: '/1',
-        templateUrl: 'views/partial1.html',
-        controller: 'MyCtrl1 as myCtrl1'
-      })
-      .state('lang.view.second', {
-        url: '/2',
-        templateUrl: 'views/partial2.html',
-        controller: 'MyCtrl2 as myCtrl2'
+    function createRouting() {
+      var defaultLang = 'en';
+      $urlRouterProvider.otherwise('/' + defaultLang + '/view/1');
+
+      $stateProvider
+        .state('lang', {
+          abstract: true,
+          url: '/:lang',
+          controller: 'LangCtrl as langCtrl',
+          template: '<ui-view/>'
+        })
+        .state('lang.view', {
+          abstract: true,
+          url: '/view',
+          template: '<ui-view/>'
+        })
+        .state('lang.view.id', {
+          url: '/:id',
+          templateUrl: getTemplateUrl,
+          controllerProvider: ['$stateParams', getCtrlName]
+        });
+
+      function getTemplateUrl($stateParams) {
+        var url = 'views/partial' + $stateParams.id + '.html';
+        return url;
+      }
+
+      function getCtrlName($stateParams) {
+        var id = $stateParams.id;
+        var ctrlName = 'MyCtrl' + id + ' as myCtrl' + id;
+        return ctrlName;
+      }
+    }
+
+    function createI18n() {
+      $translateProvider.translations('en', {
+        'TITLE': 'Hello',
+        'FOO': 'This is a paragraph'
       });
 
-    $translateProvider.translations('en', {
-      'TITLE': 'Hello',
-      'FOO': 'This is a paragraph'
-    });
+      $translateProvider.translations('de', {
+        'TITLE': 'Hallo',
+        'FOO': 'Dies ist ein Paragraph'
+      });
 
-    $translateProvider.translations('de', {
-      'TITLE': 'Hallo',
-      'FOO': 'Dies ist ein Paragraph'
-    });
+      $translateProvider.translations('ru', {
+        'TITLE': 'Привет',
+        'FOO': 'Это параграф'
+      });
 
-    $translateProvider.translations('ru', {
-      'TITLE': 'Привет',
-      'FOO': 'Это параграф'
-    });
-
-    $translateProvider.preferredLanguage('en');
+      $translateProvider.preferredLanguage('en');
+    }
   }
 })();

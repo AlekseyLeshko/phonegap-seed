e162eaaba4bf78f135ffae050d05c9724c601f6f
alekseyleshko <alekseyleshko@gmail.com>
2014-09-19 18:22:37 +0400

    Update myCtrl1

--- app/controllers/myCtrl1.controller.js
+++ app/controllers/myCtrl1.controller.js
@@ -5,20 +5,15 @@
     .module('app')
     .controller('MyCtrl1' , MyCtrl1);
 
-  MyCtrl1.$inject = ['$translate'];
+  MyCtrl1.$inject = [];
 
-  function MyCtrl1($translate) {
+  function MyCtrl1() {
     var vm = this;
     vm.msg = 'Hello, ';
     vm.click = click;
-    vm.setLang = setLang;
 
     function click() {
       vm.msg += ' world!';
-    }
-
-    function setLang(langKey) {
-      $translate.use(langKey);
     }
   }
 })();

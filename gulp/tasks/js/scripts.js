07375a168a99e9d83d51cbf640dd5ded86b7d354
alekseyleshko <alekseyleshko@gmail.com>
2014-09-22 15:12:18 +0400

    Update dependencies

--- gulp/tasks/js/scripts.js
+++ gulp/tasks/js/scripts.js
@@ -3,7 +3,6 @@ var appConfig = require('../../../config/app.json');
 var angularFilesort = require('gulp-angular-filesort');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
 var javascriptobfuscator = require('gulp-javascriptobfuscator');
 
 gulp.task('scripts', ['jshint'], function() {
@@ -13,10 +12,8 @@ gulp.task('scripts', ['jshint'], function() {
 
   return gulp.src('app/**/*.js')
     .pipe(angularFilesort())
-    .pipe(sourcemaps.init())
     .pipe(uglify(config))
     .pipe(concat(appConfig.appScript))
-    .pipe(sourcemaps.write('maps'))
     .pipe(javascriptobfuscator())
     .pipe(gulp.dest('www/js/'));
 });

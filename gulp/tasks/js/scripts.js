f3bd1232bd4d235957694246e13c882bed5a1059
alekseyleshko <alekseyleshko@gmail.com>
2014-09-22 11:28:40 +0400

    Add gulp-angular-filesort plugin and configure him

--- gulp/tasks/js/scripts.js
+++ gulp/tasks/js/scripts.js
@@ -1,19 +1,17 @@
 var gulp = require('gulp');
 var appConfig = require('../../../config/app.json');
+var angularFilesort = require('gulp-angular-filesort');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', ['jshint'], function() {
-  var paths = [
-    'app.module.js',
-    'app/**/*.js'
-  ];
   var config = {
     mangle: true
   };
 
-  return gulp.src(paths)
+  return gulp.src('app/**/*.js')
+    .pipe(angularFilesort())
     .pipe(sourcemaps.init())
     .pipe(uglify(config))
     .pipe(concat(appConfig.appScript))

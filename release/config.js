53344a5eccca4c1d80d7c70817ea3d3ab1d3d4b8
alekseyleshko <alekseyleshko@gmail.com>
2014-09-26 15:29:23 +0400

    Update config js class

--- release/config.js
+++ release/config.js
@@ -4,20 +4,38 @@ var bail = require('./bail');
 
 Config = function(fileName) {
   this.fileName = fileName;
-  this.file = fs.readFileSync(fileName);
-  this.json = JSON.parse(this.file);
+  this.readFile();
 }
 
 Config.prototype = {
+  readFile: function() {
+    this.file = fs.readFileSync(this.fileName);
+    this.json = JSON.parse(this.file);
+  },
+
   check: function () {
     if (!('version' in this.json)) {
       bail('ERROR: Could not find version in ' + this.fileName);
     }
 
-    this.ver = semver.valid(this.json.version);
+    this.ver = semver.parse(this.json.version);
     if (this.ver === null) {
       bail('ERROR: Incorrect version in ' + this.fileName);
     }
+  },
+
+  incVer: function(type) {
+    var defaultType = "patch";
+    var newVer = this.ver.inc(defaultType);
+    this.json.version = newVer.toString();
+  },
+
+  save: function() {
+    var replacer = undefined;
+    var space = 2;
+    var jsonData = JSON.stringify(this.json, replacer, space);
+    fs.writeFileSync(this.fileName, jsonData);
+    this.readFile();
   }
 };
 

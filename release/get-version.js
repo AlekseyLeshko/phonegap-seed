54a3349288a2002c06a5be4df05c0398c02196d4
alekseyleshko <alekseyleshko@gmail.com>
2014-09-30 12:56:29 +0400

    Update get-version script

--- release/get-version.js
+++ release/get-version.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var semver = require("semver");
 
-var packageFile = fs.readFileSync("package.json");
+var packageFile = fs.readFileSync('./config/app.json');
 var packageJson = JSON.parse(packageFile);
-var ver = semver.parse(packageJson.version);
+var ver = semver.parse(packageJson.config.version);
 console.log(ver.toString());

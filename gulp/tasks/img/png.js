ea1e1ba17c6f5baa27c4131ecac6a86c08960121
alekseyleshko <alekseyleshko@gmail.com>
2014-10-30 15:08:22 +0300

    Fix

--- gulp/tasks/img/png.js
+++ gulp/tasks/img/png.js
@@ -3,6 +3,6 @@ var optipng = require('imagemin-optipng');
 
 gulp.task('png', function() {
   return gulp.src('app/assets/img/**/*.png')
-    .pipe(optipng({ optimizationLevel: 3 }))
+    .pipe(optipng({ optimizationLevel: 3 })())
     .pipe(gulp.dest('www/img'));
 });

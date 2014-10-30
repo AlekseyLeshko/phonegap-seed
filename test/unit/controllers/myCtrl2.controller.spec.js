5a65322e4d66efd37a28aa90653e4c241fcfd31c
alekseyleshko <alekseyleshko@gmail.com>
2014-10-30 14:03:29 +0300

    Update test

--- test/unit/controllers/myCtrl2.controller.spec.js
+++ test/unit/controllers/myCtrl2.controller.spec.js
@@ -3,16 +3,38 @@
 describe('MyCtrl2 controller', function() {
   beforeEach(module('app'));
 
-  var ctrlName = 'MyCtrl2';
   var ctrl;
+  var strBuilderService;
+  var strBuilderFactory;
 
   beforeEach(function() {
+    var ctrlName = 'MyCtrl2';
+
+    var buildHello = function (msg) {
+      return 'Hello, ' + msg;
+    };
+    strBuilderService = {
+      buildHello: buildHello
+    };
+    strBuilderFactory = {
+      buildHello: buildHello
+    };
+
+    spyOn(strBuilderService, 'buildHello').and.callThrough();
+    spyOn(strBuilderFactory, 'buildHello').and.callThrough();
+
+    var injectObj = {
+      strBuilderService: strBuilderService,
+      strBuilderFactory: strBuilderFactory
+    };
     inject(function($injector, $controller) {
-      ctrl = $controller(ctrlName);
+      ctrl = $controller(ctrlName, injectObj);
     });
   });
 
   it('should init', function() {
+    expect(strBuilderService.buildHello).toHaveBeenCalled();
+    expect(strBuilderFactory.buildHello).toHaveBeenCalled();
     expect(ctrl.msg).toEqual('Hello, World! : Hello, World!');
   });
 });

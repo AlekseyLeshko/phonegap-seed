2630e72d68b5eb8248ee0a3e2e348781cb62a8cc
alekseyleshko <alekseyleshko@gmail.com>
2014-10-31 11:29:36 +0300

    Fix test

--- test/unit/directives/directives.spec.js
+++ test/unit/directives/directives.spec.js
@@ -1,17 +1,15 @@
 'use strict';
 
-describe('directives', function() {
+describe('app-version directive', function() {
   beforeEach(module('app'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
+  it('should print current version', function() {
+    module(function($provide) {
+      $provide.value('version', 'TEST_VER');
+    });
+    inject(function($compile, $rootScope) {
+      var element = $compile('<span app-version></span>')($rootScope);
+      expect(element.text()).toEqual('TEST_VER');
     });
   });
 });
